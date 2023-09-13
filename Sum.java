be1986d5513b1d22f768dbde845f3c2192c256ab
Emmanuel <chukwuebukaoluwatobi@gmail.com>
2023-09-13 10:39:57 -0700

    New committed file

---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum{
+    public static int summation(int a, int b){
+          int result;
+          result = a+b;
+          return result;
+     }
+     public static void main(String[] args){
+        int answer;
+          answer=summation(50, 75);
+          System.out.println(answer);
+     }
+}
---
+++ javaFunction.java
@@ -0,0 +1,10 @@
+public class javaFunction{
+    public static int multiply(int x, int y){
+        int result = x*y;
+        return result;
+    }
+    public static void main(String[] args){
+        System.out.println(multiply(10,42));
+        
+    }
+}
